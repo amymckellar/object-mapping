ac4c2f2bc45766fd193b42fed4ed5060c3bd4440
amymckellar <amy_mckellar@yahoo.com>
2023-03-19 20:38:40 -0500

    create a new category and update category in the category-routes.js file

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -31,12 +31,30 @@ router.get('/:id', (req, res) => {
 
 });
 
+// create a new category
 router.post('/', (req, res) => {
-  // create a new category
-});
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 
+});
+// update a category by its `id` value
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!categoryData[0]) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  }
 });
 
 router.delete('/:id', (req, res) => {
