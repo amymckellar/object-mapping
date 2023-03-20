a17694bb26cacf5d5c1e5ef80d205380ff949a16
amymckellar <amy_mckellar@yahoo.com>
2023-03-19 20:25:50 -0500

    adding products belongsto category and categories have many products in the index.js file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,12 +1,18 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+products.belongsTo(category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+category.hasMany(products, {
+  foreignKey: "category_id",
+});
 
 // Products belongToMany Tags (through ProductTag)
 
